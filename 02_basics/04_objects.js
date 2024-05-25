a5abb28590abd95f7c941930b1a0f417f4bbad65
Priyanka-Singh3 <141346628+Priyanka-Singh3@users.noreply.github.com>
2024-05-25 17:28:28 +0000

    destructuring and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,9 +25,28 @@ const user = [
 ] //array of objects
 
 //to get the keys
-console.log(Object.keys(obj1)) //as an array
+// console.log(Object.keys(obj1)) //as an array
 
 //to get the values
-console.log(Object.values(obj1)) //as an array
+// console.log(Object.values(obj1)) //as an array
 
-console.log(Object.entries(obj1)) //array of array of key and values
+// console.log(Object.entries(obj1)) //array of array of key and values
+
+//de-structuring
+const course = {
+    cname: "dsa",
+    ccost: 600,
+    cinstructor: "Shradha didi"
+}
+
+const {cinstructor: inst} = course
+console.log(inst)
+
+//data from backend comes in the form of json  (api data is in the form of json)
+/* 
+json format
+{
+    "name" : "Pri",
+    "email" : "pri1062003@gmail.com"
+}
+*/
