a2a4103728511fb6c722bc081719c87460a59650
Priyanka-Singh3 <141346628+Priyanka-Singh3@users.noreply.github.com>
2024-05-25 17:11:59 +0000

    objects second video

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+const newObj = new Object() //singleton object
+
+//const Obj = {} this is non-singleton object
+
+//object can also exist as an key inside that object => nested objects
+
+//we can merge objects also
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = Object.assign({}, obj1, obj2)
+
+const obj4 = {...obj1, ...obj2} //spread
+
+const user = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+] //array of objects
+
+//to get the keys
+console.log(Object.keys(obj1)) //as an array
+
+//to get the values
+console.log(Object.values(obj1)) //as an array
+
+console.log(Object.entries(obj1)) //array of array of key and values
